e01c7306528b53578db0a2685b2d9a2cd301cfe3
Ankita Kumari <ankitakumari3503@gmail.com>
2023-06-16 18:19:50 +0530

    Add files via upload

---
+++ Data Type.js
@@ -0,0 +1,27 @@
+// Data Types in JavaScript
+
+//Primitive data types
+let a=24
+let b=null
+
+let c="Ankita";
+let d="Symbol"
+
+let e=true
+let f=BigInt("2749832039")
+
+let g // or let g=undefined
+
+console.log(a,b,c,d,e,f,g)
+
+//Non-primitive data type
+const people={
+    firstName: "Ankita",
+    lastName: "Kumari",
+    age: 34
+}
+
+console.log(people)
+console.log(people["firstName"]) 
+console.log(people.age)
+
---
+++ Variables.js
@@ -0,0 +1,16 @@
+var a=10
+var a="Ankita"
+console.log(a)   
+// As we know that var  values can be updated and re-assigned too
+
+let b=69
+{
+let b="A"       
+}
+console.log(b)
+
+// As we know that let is block spaced it can't be redeclared
+
+const pi=3.14
+//pi=345   // It cannot be redeclared
+console.log(pi) 
