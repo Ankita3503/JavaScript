a49885e15a8fe2cba9490a341d9ba209cedbc830
Ankita Kumari <ankitakumari3503@gmail.com>
2023-07-04 17:09:48 +0530

    Update Data Type.js

--- Data Type.js
+++ Data Type.js
@@ -1,16 +1,16 @@
 // Data Types in JavaScript
 
 //Primitive data types
-let a=24
-let b=null
+let a=24   //number
+let b=null  //null
 
-let c="Ankita";
-let d="Symbol"
+let c="Ankita";  //string
+let d="Symbol"  //symbol
 
-let e=true
-let f=BigInt("2749832039")
+let e=true    /boolean
+let f=BigInt("2749832039")  // bigint
 
-let g // or let g=undefined
+let g // or let g=undefined   // undefined
 
 console.log(a,b,c,d,e,f,g)
 
